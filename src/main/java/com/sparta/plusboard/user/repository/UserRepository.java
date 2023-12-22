f70f11934b5d1ca8a393b5ceb4d663f37f3fc580
yeongdo99 <atpsfka1@gmail.com>
2023-12-22 12:17:05 +0900

    feat : UserRepository 구현

---
+++ src/main/java/com/sparta/plusboard/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.plusboard.user.repository;
+
+import com.sparta.plusboard.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
