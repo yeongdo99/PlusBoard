1f050a249ba5d8da151816c6addaf7e76694fc3d
yeongdo99 <atpsfka1@gmail.com>
2023-12-22 12:19:29 +0900

    feat : User Entity 구현

---
+++ src/main/java/com/sparta/plusboard/user/entity/User.java
@@ -0,0 +1,31 @@
+package com.sparta.plusboard.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
